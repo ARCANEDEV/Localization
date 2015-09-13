6f1d20cdb8edf987c47fc7b4bb8ae09142dc96c0
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-13 02:47:08 +0100

    Adding Localization base class

---
+++ src/Contracts/LocalizationInterface.php
@@ -0,0 +1,12 @@
+<?php namespace Arcanedev\Localization\Contracts;
+
+/**
+ * Interface  LocalizationInterface
+ *
+ * @package   Arcanedev\Localization\Contracts
+ * @author    ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+interface LocalizationInterface
+{
+
+}
---
+++ src/Localization.php
@@ -0,0 +1,14 @@
+<?php namespace Arcanedev\Localization;
+
+use Arcanedev\Localization\Contracts\LocalizationInterface;
+
+/**
+ * Class     Localization
+ *
+ * @package  Arcanedev\Localization
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class Localization implements LocalizationInterface
+{
+    //
+}
