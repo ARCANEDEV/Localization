8f9a633a7e0a062a7a5d07b43b78edba62ef8d20
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-13 18:13:36 +0100

    Adding Localization helper

--- helpers.php
+++ helpers.php
@@ -1,2 +1,13 @@
 <?php
-//
+
+if ( ! function_exists('localization')) {
+    /**
+     * Get the Localization instance.
+     *
+     * @return \Arcanedev\Localization\Localization
+     */
+    function localization()
+    {
+        return app('arcanedev.localization');
+    }
+}
