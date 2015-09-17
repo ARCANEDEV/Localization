dfbfc01e9732fe876090fe85f1e75eefc9c66dc5
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-17 11:29:58 +0100

    Adding router custom methods

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -1,6 +1,7 @@
 <?php namespace Arcanedev\Localization\Routing;
 
 use Closure;
+use Illuminate\Routing\Router as IlluminateRouter;
 
 /**
  * Class     Router
@@ -8,7 +9,7 @@ use Closure;
  * @package  Arcanedev\Localization\Routing
  * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
  */
-class Router extends \Illuminate\Routing\Router
+class Router extends IlluminateRouter
 {
     /* ------------------------------------------------------------------------------------------------
      |  Route Functions
@@ -28,5 +29,70 @@ class Router extends \Illuminate\Routing\Router
         ]);
 
         $this->group($attributes, $callback);
+    }
+
+    /**
+     * @param  string                 $trans
+     * @param  \Closure|array|string  $action
+     *
+     * @return \Illuminate\Routing\Route
+     */
+    public function transGet($trans, $action)
+    {
+    }
+
+    /**
+     * @param  string                 $trans
+     * @param  \Closure|array|string  $action
+     *
+     * @return \Illuminate\Routing\Route
+     */
+    public function transPost($trans, $action)
+    {
+    }
+
+    /**
+     * @param  string                 $trans
+     * @param  \Closure|array|string  $action
+     *
+     * @return \Illuminate\Routing\Route
+     */
+    public function transPut($trans, $action)
+    {
+    }
+
+    /**
+     * Register a new PATCH route with the router.
+     *
+     * @param  string                 $trans
+     * @param  \Closure|array|string  $action
+     *
+     * @return \Illuminate\Routing\Route
+     */
+    public function transPatch($trans, $action)
+    {
+
+    }
+
+    /**
+     * @param  string                 $trans
+     * @param  \Closure|array|string  $action
+     *
+     * @return \Illuminate\Routing\Route
+     */
+    public function transDelete($trans, $action)
+    {
+    }
+
+    /**
+     * Register a new OPTIONS route with the router.
+     *
+     * @param  string                 $trans
+     * @param  \Closure|array|string  $action
+     *
+     * @return \Illuminate\Routing\Route
+     */
+    public function transOptions($trans, $action)
+    {
     }
 }
