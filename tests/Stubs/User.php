03268bb64b106d424ce238f2f1395cdc38f9d37e
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-13 18:14:59 +0000

    Testing the Url Utility class for route bindings fix

---
+++ tests/Stubs/User.php
@@ -0,0 +1,51 @@
+<?php namespace Arcanedev\Localization\Tests\Stubs;
+
+use Arcanedev\Localization\Contracts\RouteBindable;
+
+/**
+ * Class     User
+ *
+ * @package  Arcanedev\Localization\Tests\Stubs
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class User implements RouteBindable
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * User's username.
+     *
+     * @var string
+     */
+    public $username;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * User constructor.
+     *
+     * @param  string  $username
+     */
+    public function __construct($username)
+    {
+        $this->username = $username;
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get the wildcard value from the class.
+     *
+     * @return int|string
+     */
+    public function getWildcardValue()
+    {
+        return $this->username;
+    }
+}
