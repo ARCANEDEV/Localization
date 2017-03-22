0ba0fe689cd68435dd453f06af73e55b9e60247c
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-03-22 19:14:14 +0000

    Revert the Kernel's Trait

--- src/Traits/LocalizationKernelTrait.php
+++ src/Traits/LocalizationKernelTrait.php
@@ -6,9 +6,38 @@
  * @package  Arcanedev\Localization\Traits
  * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
  *
- * @deprecated since v1.0.3
+ * @property  \Illuminate\Foundation\Application      app
+ * @property  \Arcanedev\Localization\Routing\Router  router
+ * @property  array                                   middlewareGroups
+ * @property  array                                   routeMiddleware
  */
 trait LocalizationKernelTrait
 {
-    //
+    /**
+     * Get the route dispatcher callback.
+     *
+     * @return \Closure
+     */
+    protected function dispatchToRouter()
+    {
+        $this->replaceRouter();
+
+        return parent::dispatchToRouter();
+    }
+
+    /**
+     * Replace the illuminate router with the localization router.
+     */
+    protected function replaceRouter()
+    {
+        $this->router = $this->app->make('router');
+
+        foreach ($this->middlewareGroups as $key => $middleware) {
+            $this->router->middlewareGroup($key, $middleware);
+        }
+
+        foreach ($this->routeMiddleware as $key => $middleware) {
+            $this->router->aliasMiddleware($key, $middleware);
+        }
+    }
 }
