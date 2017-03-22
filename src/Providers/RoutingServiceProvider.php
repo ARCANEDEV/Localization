8fdd6fd6c0de1fb093591e7ef3be44b15e23b401
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-03-22 17:12:42 +0000

    Updating the router

--- src/Providers/RoutingServiceProvider.php
+++ src/Providers/RoutingServiceProvider.php
@@ -1,7 +1,7 @@
 <?php namespace Arcanedev\Localization\Providers;
 
 use Arcanedev\Localization\Middleware;
-use Illuminate\Routing\RoutingServiceProvider as ServiceProvider;
+use Arcanedev\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Arcanedev\Localization\Routing\Router;
 
 /**
@@ -12,16 +12,24 @@ use Arcanedev\Localization\Routing\Router;
  */
 class RoutingServiceProvider extends ServiceProvider
 {
-    /* ------------------------------------------------------------------------------------------------
+    /* -----------------------------------------------------------------
      |  Properties
-     | ------------------------------------------------------------------------------------------------
+     | -----------------------------------------------------------------
      */
     /**
      * The application's route middleware.
      *
+     * These middleware may be assigned to groups or used individually.
+     *
      * @var array
      */
-    protected $middleware = [];
+    protected $routeMiddleware = [
+        'localization-session-redirect' => Middleware\LocaleSessionRedirect::class,
+        'localization-cookie-redirect'  => Middleware\LocaleCookieRedirect::class,
+        'localization-redirect'         => Middleware\LocalizationRedirect::class,
+        'localized-routes'              => Middleware\LocalizationRoutes::class,
+        'translation-redirect'          => Middleware\TranslationRedirect::class,
+    ];
 
     /* ------------------------------------------------------------------------------------------------
      |  Main Functions
@@ -29,12 +37,12 @@ class RoutingServiceProvider extends ServiceProvider
      */
     /**
      * Register the service provider.
-     *
-     * @return void
      */
     public function register()
     {
         $this->registerRouter();
+
+        parent::register();
     }
 
     /* ------------------------------------------------------------------------------------------------
@@ -49,58 +57,5 @@ class RoutingServiceProvider extends ServiceProvider
         $this->app->singleton('router', function ($app) {
             return new Router($app['events'], $app);
         });
-
-        $this->registerMiddlewares();
-    }
-
-    /**
-     * Register the middlewares.
-     */
-    private function registerMiddlewares()
-    {
-        /** @var Router $router */
-        $router      = $this->app['router'];
-        $middlewares = [
-            'localization-session-redirect' => Middleware\LocaleSessionRedirect::class,
-            'localization-cookie-redirect'  => Middleware\LocaleCookieRedirect::class,
-            'localization-redirect'         => Middleware\LocalizationRedirect::class,
-            'localized-routes'              => Middleware\LocalizationRoutes::class,
-            'translation-redirect'          => Middleware\TranslationRedirect::class,
-        ];
-
-        foreach ($middlewares as $name => $class) {
-            $this->registerMiddleware($router, $name, $class);
-        }
-    }
-
-    /**
-     * Register a middleware.
-     *
-     * @param  Router  $router
-     * @param  string  $name
-     * @param  string  $class
-     */
-    private function registerMiddleware(Router $router, $name, $class)
-    {
-        $router->aliasMiddleware($name, $class);
-
-        if ($this->getMiddleware($name)) {
-            $this->middleware[] = $name;
-        }
-    }
-
-    /**
-     * Get the middleware status.
-     *
-     * @param  string  $name
-     *
-     * @return bool
-     */
-    private function getMiddleware($name)
-    {
-        /** @var  \Illuminate\Contracts\Config\Repository  $config */
-        $config = $this->app['config'];
-
-        return (bool) $config->get("localization.route.middleware.{$name}", false);
     }
 }
